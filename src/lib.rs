2044ba2ba7a8bab925a476237b17be7275d1a6e3
Uwe Klotz <uwe.klotz@slowtec.de>
2018-12-13 14:46:14 +0100

    Use the uom crate for measurement values

--- src/lib.rs
+++ src/lib.rs
@@ -3,32 +3,59 @@ pub mod modbus;
 
 use futures::Future;
 use std::{fmt, io::Error};
+use uom::si::f64;
 
 #[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
-/// Temperature (°C).
-pub struct Temperature {
-    pub celsius: f64,
+/// /Thermodynamic) Temperature.
+pub struct Temperature(f64::ThermodynamicTemperature);
+
+impl Temperature {
+    pub /*const*/ fn from_degree_celsius(degree_celsius: f64) -> Self {
+        Temperature(f64::ThermodynamicTemperature::new::<uom::si::thermodynamic_temperature::degree_celsius>(degree_celsius))
+    }
+
+    pub fn degree_celsius(self) -> f64 {
+        self.0.get::<uom::si::thermodynamic_temperature::degree_celsius>()
+    }
+}
+
+impl From<f64::ThermodynamicTemperature> for Temperature {
+    fn from(from: f64::ThermodynamicTemperature) -> Self {
+        Temperature(from)
+    }
+}
+
+impl From<Temperature> for f64::ThermodynamicTemperature {
+    fn from(from: Temperature) -> Self {
+        from.0
+    }
 }
 
 impl fmt::Display for Temperature {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} °C", self.celsius)
+        write!(f, "{}°C", self.degree_celsius())
     }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
 /// Soil volumetric water content (VWC).
-pub struct WaterContent {
-    pub percent: f64,
-}
+pub struct VolumetricWaterContent(f64::Ratio);
 
-impl WaterContent {
-    pub const fn min() -> Self {
-        Self { percent: 0.0 }
+impl VolumetricWaterContent {
+    pub /*const*/ fn from_percent(percent: f64) -> Self {
+        VolumetricWaterContent(f64::Ratio::new::<uom::si::ratio::percent>(percent))
     }
 
-    pub const fn max() -> Self {
-        Self { percent: 100.0 }
+    pub fn percent(self) -> f64 {
+        self.0.get::<uom::si::ratio::percent>()
+    }
+
+    pub /*const*/ fn min() -> Self {
+        Self::from_percent(0.0)
+    }
+
+    pub /*const*/ fn max() -> Self {
+        Self::from_percent(100.0)
     }
 
     pub fn is_valid(self) -> bool {
@@ -36,21 +63,39 @@ impl WaterContent {
     }
 }
 
-impl fmt::Display for WaterContent {
+impl From<f64::Ratio> for VolumetricWaterContent {
+    fn from(from: f64::Ratio) -> Self {
+        VolumetricWaterContent(from)
+    }
+}
+
+impl From<VolumetricWaterContent> for f64::Ratio {
+    fn from(from: VolumetricWaterContent) -> Self {
+        from.0
+    }
+}
+
+impl fmt::Display for VolumetricWaterContent {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} %", self.percent)
+        write!(f, "{}%", self.percent())
     }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
 /// Relative permittivity or dielectric constant (DK).
-pub struct RelativePermittivity {
-    pub ratio: f64,
-}
+pub struct RelativePermittivity(f64::Ratio);
 
 impl RelativePermittivity {
-    pub const fn min() -> Self {
-        Self { ratio: 1.0 }
+    pub /*const*/ fn from_ratio(ratio: f64) -> Self {
+        RelativePermittivity(f64::Ratio::new::<uom::si::ratio::ratio>(ratio))
+    }
+
+    pub fn ratio(self) -> f64 {
+        self.0.get::<uom::si::ratio::ratio>()
+    }
+
+    pub /*const*/ fn min() -> Self {
+        Self::from_ratio(1.0)
     }
 
     pub fn is_valid(self) -> bool {
@@ -58,9 +103,21 @@ impl RelativePermittivity {
     }
 }
 
+impl From<f64::Ratio> for RelativePermittivity {
+    fn from(from: f64::Ratio) -> Self {
+        RelativePermittivity(from)
+    }
+}
+
+impl From<RelativePermittivity> for f64::Ratio {
+    fn from(from: RelativePermittivity) -> Self {
+        from.0
+    }
+}
+
 impl fmt::Display for RelativePermittivity {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.ratio)
+        write!(f, "{}", self.ratio())
     }
 }
 
@@ -72,11 +129,28 @@ pub trait Sensor {
 
     /// Measure the current water content of the medium (soil) around the sensor
     /// in the range from 0% to 60% (up to 100% with limited accuracy).
-    fn read_water_content(&self) -> Box<Future<Item = WaterContent, Error = Error>>;
+    fn read_water_content(&self) -> Box<Future<Item = VolumetricWaterContent, Error = Error>>;
 
     /// Measure the current (relative) permittivity of the medium around the sensor.
     fn read_permittivity(&self) -> Box<Future<Item = RelativePermittivity, Error = Error>>;
 
     /// Retrieve the current raw and uncalibrated signal of the sensor.
     fn read_counts(&self) -> Box<Future<Item = usize, Error = Error>>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn water_content_percent() {
+        for i in 0..=100 {
+            assert!(VolumetricWaterContent::from_percent(i as f64).is_valid());
+            assert!(
+                (i as f64 - VolumetricWaterContent::from_percent(i as f64).percent()).abs() < 0.000001,
+            );
+        }
+        assert!(!VolumetricWaterContent::from_percent(-0.5).is_valid());
+        assert!(!VolumetricWaterContent::from_percent(100.01).is_valid());
+    }
 }
--- src/modbus/mod.rs
+++ src/modbus/mod.rs
@@ -13,8 +13,8 @@ struct TemperatureRaw(u16);
 
 impl From<TemperatureRaw> for Temperature {
     fn from(from: TemperatureRaw) -> Self {
-        let celsius = f64::from(i32::from(from.0) - 10000i32) / 100f64;
-        Self { celsius }
+        let degree_celsius = f64::from(i32::from(from.0) - 10000i32) / 100f64;
+        Self::from_degree_celsius(degree_celsius)
     }
 }
 
@@ -25,19 +25,19 @@ pub fn decode_temperature_bytes(bytes: &[u8]) -> Result<Temperature> {
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-struct WaterContentRaw(u16);
+struct VolumetricWaterContentRaw(u16);
 
-impl From<WaterContentRaw> for WaterContent {
-    fn from(from: WaterContentRaw) -> Self {
+impl From<VolumetricWaterContentRaw> for VolumetricWaterContent {
+    fn from(from: VolumetricWaterContentRaw) -> Self {
         let percent = f64::from(from.0) / 100f64;
-        Self { percent }
+        Self::from_percent(percent)
     }
 }
 
-pub fn decode_water_content_bytes(bytes: &[u8]) -> Result<WaterContent> {
+pub fn decode_water_content_bytes(bytes: &[u8]) -> Result<VolumetricWaterContent> {
     let mut rdr = Cursor::new(bytes);
     let raw = rdr.read_u16::<BigEndian>()?;
-    let res: WaterContent = WaterContentRaw(raw).into();
+    let res: VolumetricWaterContent = VolumetricWaterContentRaw(raw).into();
     if res.is_valid() {
         Ok(res)
     } else {
@@ -54,7 +54,7 @@ struct RelativePermittivityRaw(u16);
 impl From<RelativePermittivityRaw> for RelativePermittivity {
     fn from(from: RelativePermittivityRaw) -> Self {
         let ratio = f64::from(from.0) / 100f64;
-        Self { ratio }
+        Self::from_ratio(ratio)
     }
 }
 
@@ -94,12 +94,12 @@ impl Context {
     }
 
     /// Implementation of Sensor::read_water_content()
-    pub fn read_water_content(&self) -> impl Future<Item = WaterContent, Error = Error> {
+    pub fn read_water_content(&self) -> impl Future<Item = VolumetricWaterContent, Error = Error> {
         self.ctx
             .read_holding_registers(0x0001, 0x0001)
             .and_then(|rsp| {
                 if let [raw] = rsp[..] {
-                    let val = WaterContent::from(WaterContentRaw(raw));
+                    let val = VolumetricWaterContent::from(VolumetricWaterContentRaw(raw));
                     if val.is_valid() {
                         Ok(val)
                     } else {
@@ -177,7 +177,7 @@ impl Sensor for Context {
         Box::new(self.read_temperature())
     }
 
-    fn read_water_content(&self) -> Box<Future<Item = WaterContent, Error = Error>> {
+    fn read_water_content(&self) -> Box<Future<Item = VolumetricWaterContent, Error = Error>> {
         Box::new(self.read_water_content())
     }
 
@@ -197,23 +197,23 @@ mod tests {
     #[test]
     fn decode_temperature() {
         assert_eq!(
-            Temperature { celsius: -40.0 },
+            Temperature::from_degree_celsius(-40.0),
             decode_temperature_bytes(&[0x17, 0x70]).unwrap()
         );
         assert_eq!(
-            Temperature { celsius: 0.0 },
+            Temperature::from_degree_celsius(0.0),
             decode_temperature_bytes(&[0x27, 0x10]).unwrap()
         );
         assert_eq!(
-            Temperature { celsius: 27.97 },
+            Temperature::from_degree_celsius(27.97),
             decode_temperature_bytes(&[0x31, 0xFD]).unwrap()
         );
         assert_eq!(
-            Temperature { celsius: 60.0 },
+            Temperature::from_degree_celsius(60.0),
             decode_temperature_bytes(&[0x3E, 0x80]).unwrap()
         );
         assert_eq!(
-            Temperature { celsius: 80.0 },
+            Temperature::from_degree_celsius(80.0),
             decode_temperature_bytes(&[0x46, 0x50]).unwrap()
         );
     }
@@ -222,15 +222,15 @@ mod tests {
     fn decode_water_content() {
         // Valid range
         assert_eq!(
-            WaterContent { percent: 0.00 },
+            VolumetricWaterContent::from_percent(0.0),
             decode_water_content_bytes(&[0x00, 0x00]).unwrap()
         );
         assert_eq!(
-            WaterContent { percent: 34.4 },
+            VolumetricWaterContent::from_percent(34.4),
             decode_water_content_bytes(&[0x0D, 0x70]).unwrap()
         );
         assert_eq!(
-            WaterContent { percent: 100.0 },
+            VolumetricWaterContent::from_percent(100.0),
             decode_water_content_bytes(&[0x27, 0x10]).unwrap()
         );
         // Invalid range
@@ -242,11 +242,11 @@ mod tests {
     fn decode_permittivity() {
         // Valid range
         assert_eq!(
-            RelativePermittivity { ratio: 1.00 },
+            RelativePermittivity::from_ratio(1.0),
             decode_permittivity_bytes(&[0x00, 0x64]).unwrap()
         );
         assert_eq!(
-            RelativePermittivity { ratio: 15.2 },
+            RelativePermittivity::from_ratio(15.2),
             decode_permittivity_bytes(&[0x05, 0xF0]).unwrap()
         );
         // Invalid range
