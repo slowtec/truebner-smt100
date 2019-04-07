2fdad44cbb7feebb19c83912c73e612cc8e1328d
Uwe Klotz <uwe.klotz@slowtec.de>
2019-04-07 15:48:48 +0200

    Refactor control loop in example

--- examples/modbus-rtu.rs
+++ examples/modbus-rtu.rs
@@ -3,11 +3,11 @@ pub fn main() {
     use chrono::{DateTime, Utc};
     use env_logger::Builder as LoggerBuilder;
     use futures::{future::Either, Future, Stream};
-    use std::{cell::RefCell, env, rc::Rc, time::Duration};
+    use std::{cell::RefCell, env, io::Error, rc::Rc, time::Duration};
     use stream_cancel::{StreamExt, Tripwire};
     use tokio::timer::Interval;
     use tokio_core::reactor::{Core, Handle};
-    use tokio_modbus::prelude::*;
+    use tokio_modbus::prelude::{client::Context as ModbusContext, *};
 
     use truebner_smt100::{modbus, *};
 
@@ -20,26 +20,30 @@ pub fn main() {
     logger_builder.init();
 
     let mut core = Core::new().unwrap();
-    let handle = core.handle();
+
+    #[derive(Debug, Clone)]
+    struct Config {
+        handle: Handle,
+        tty_path: String,
+        slave: Slave,
+        cycle_time: Duration,
+        timeout: Duration,
+    };
+
+    impl Config {
+        fn connect(&self) -> impl Future<Item = ModbusContext, Error = Error> {
+            modbus::rtu::connect_path(&self.handle, &self.tty_path)
+        }
+    }
 
     // TODO: Parse parameters and options from command-line arguments
-    let tty_path = "/dev/ttyUSB0";
-    let slave = Slave::min_device();
-    let broadcast_slave = false;
-    let cycle_time = Duration::from_millis(1000);
-    let timeout = Duration::from_millis(500);
-
-    let context_task = modbus::rtu::connect_path(&handle, tty_path);
-    let context = Rc::new(RefCell::new(core.run(context_task).unwrap()));
-
-    log::info!("Creating Modbus slave proxy for {:?}", slave);
-    let proxy = modbus::SlaveProxy::new(Rc::clone(&context), slave);
-
-    if broadcast_slave {
-        log::info!("Resetting Modbus slave address to {:?}", proxy.slave());
-        let broadcast_task = proxy.broadcast_slave();
-        core.run(broadcast_task).unwrap();
-    }
+    let config = Config {
+        handle: core.handle(),
+        tty_path: "/dev/ttyUSB0".to_owned(),
+        slave: Slave::min_device(),
+        cycle_time: Duration::from_millis(1000),
+        timeout: Duration::from_millis(500),
+    };
 
     #[derive(Debug, Clone, Copy, PartialEq, Eq)]
     struct Measurement<T> {
@@ -58,106 +62,157 @@ pub fn main() {
 
     #[derive(Debug, Default, Clone, Copy, PartialEq)]
     struct Measurements {
-        temp: Option<Measurement<Temperature>>,
-        vwc: Option<Measurement<VolumetricWaterContent>>,
-        perm: Option<Measurement<RelativePermittivity>>,
+        temperature: Option<Measurement<Temperature>>,
+        water_content: Option<Measurement<VolumetricWaterContent>>,
+        permittivity: Option<Measurement<RelativePermittivity>>,
     }
 
-    struct ControlLoopState {
-        handle: Handle,
-        tty_path: String,
+    struct State {
         proxy: modbus::SlaveProxy,
         measurements: Measurements,
     };
 
-    let (_trigger, tripwire) = Tripwire::new();
-    let state = ControlLoopState {
-        handle,
-        tty_path: tty_path.to_owned(),
-        proxy,
-        measurements: Default::default(),
+    impl State {
+        pub fn new(context: &Rc<RefCell<ModbusContext>>, slave: Slave) -> Self {
+            Self {
+                proxy: modbus::SlaveProxy::new(Rc::clone(context), slave),
+                measurements: Default::default(),
+            }
+        }
+
+        pub fn reset_context(&mut self, context: &Rc<RefCell<ModbusContext>>) {
+            self.proxy.reset_context(Rc::clone(context));
+        }
+    }
+
+    struct ControlLoop {
+        config: Config,
+        state: State,
     };
-    let cycle_interval = Interval::new_interval(cycle_time);
-    let control_loop_task = cycle_interval
+
+    impl ControlLoop {
+        pub fn connect(config: Config) -> impl Future<Item = Self, Error = Error> {
+            config.connect().map(move |context| {
+                let state = State::new(&Rc::new(RefCell::new(context)), config.slave);
+                Self { config, state }
+            })
+        }
+
+        fn reconnect(mut self) -> impl Future<Item = Self, Error = (Self, Error)> {
+            self.config.connect().then(move |res| match res {
+                Ok(context) => {
+                    self.state.reset_context(&Rc::new(RefCell::new(context)));
+                    Ok(self)
+                }
+                Err(err) => Err((self, err)),
+            })
+        }
+
+        pub fn measure_temperature(mut self) -> impl Future<Item = Self, Error = (Self, Error)> {
+            self.state
+                .proxy
+                .read_temperature(self.config.timeout)
+                .then(move |res| match res {
+                    Ok(val) => {
+                        self.state.measurements.temperature = Some(Measurement::new(val));
+                        Ok(self)
+                    }
+                    Err(err) => Err((self, err)),
+                })
+        }
+
+        pub fn measure_water_content(mut self) -> impl Future<Item = Self, Error = (Self, Error)> {
+            self.state
+                .proxy
+                .read_water_content(self.config.timeout)
+                .then(move |res| match res {
+                    Ok(val) => {
+                        self.state.measurements.water_content = Some(Measurement::new(val));
+                        Ok(self)
+                    }
+                    Err(err) => Err((self, err)),
+                })
+        }
+
+        pub fn measure_permittivity(mut self) -> impl Future<Item = Self, Error = (Self, Error)> {
+            self.state
+                .proxy
+                .read_permittivity(self.config.timeout)
+                .then(move |res| match res {
+                    Ok(val) => {
+                        self.state.measurements.permittivity = Some(Measurement::new(val));
+                        Ok(self)
+                    }
+                    Err(err) => Err((self, err)),
+                })
+        }
+
+        pub fn recover_after_error(self, err: &Error) -> impl Future<Item = Self, Error = ()> {
+            log::warn!(
+                "Reconnecting serial port {} after error: {}",
+                self.config.tty_path,
+                err
+            );
+            self.reconnect().then(move |res| {
+                let this = match res {
+                    Ok(this) => this,
+                    Err((this, err)) => {
+                        log::error!(
+                            "Failed to reconnect serial port {}: {}",
+                            this.config.tty_path,
+                            err
+                        );
+                        // Continue and don't leave/terminate the control loop!
+                        this
+                    }
+                };
+                Ok(this)
+            })
+        }
+
+        pub fn broadcast_slave(&self) -> impl Future<Item = (), Error = Error> {
+            self.state.proxy.broadcast_slave()
+        }
+    }
+
+    log::info!("Connecting: {:?}", config);
+    let ctrl_loop = core.run(ControlLoop::connect(config)).unwrap();
+
+    let broadcast_slave = false;
+    if broadcast_slave {
+        log::info!(
+            "Resetting Modbus slave address to {:?}",
+            ctrl_loop.state.proxy.slave()
+        );
+        core.run(ctrl_loop.broadcast_slave()).unwrap();
+    }
+
+    let (_trigger, tripwire) = Tripwire::new();
+    let cycle_interval = Interval::new_interval(ctrl_loop.config.cycle_time);
+    let ctrl_loop_task = cycle_interval
         .map_err(|err| {
             log::error!("Aborting control loop after timer error: {:?}", err);
         })
         .take_until(tripwire)
-        .fold(state, |mut state, _event| {
+        .fold(ctrl_loop, |ctrl_loop, _event| {
             // Asynchronous chain of measurements
-            state
-                .proxy
-                .read_temperature(timeout)
-                .then(move |res| match res {
-                    Ok(val) => {
-                        state.measurements.temp = Some(Measurement::new(val));
-                        Ok(state)
+            futures::future::ok(ctrl_loop)
+                .and_then(ControlLoop::measure_temperature)
+                .and_then(ControlLoop::measure_water_content)
+                .and_then(ControlLoop::measure_permittivity)
+                .then(|res| match res {
+                    Ok(ctrl_loop) => {
+                        log::info!("{:?}", ctrl_loop.state.measurements);
+                        Either::A(futures::future::ok(ctrl_loop))
                     }
-                    Err(err) => Err((state, err)),
-                })
-                .and_then(|mut state| {
-                    state
-                        .proxy
-                        .read_water_content(timeout)
-                        .then(move |res| match res {
-                            Ok(val) => {
-                                state.measurements.vwc = Some(Measurement::new(val));
-                                Ok(state)
-                            }
-                            Err(err) => Err((state, err)),
-                        })
-                })
-                .and_then(|mut state| {
-                    state
-                        .proxy
-                        .read_permittivity(timeout)
-                        .then(move |res| match res {
-                            Ok(val) => {
-                                state.measurements.perm = Some(Measurement::new(val));
-                                Ok(state)
-                            }
-                            Err(err) => Err((state, err)),
-                        })
-                })
-                .then(|res| {
-                    match res {
-                        Ok(state) => {
-                            log::info!("{:?}", state.measurements);
-                            Either::A(futures::future::ok(state))
-                        }
-                        Err((mut state, err)) => {
-                            log::warn!(
-                                "Reconnecting serial port {} after error: {}",
-                                state.tty_path,
-                                err
-                            );
-                            let reconnected_state = modbus::rtu::connect_path(
-                                &state.handle,
-                                &state.tty_path,
-                            )
-                            .then(move |res| {
-                                match res {
-                                    Ok(context) => {
-                                        state.proxy.reset_context(Rc::new(RefCell::new(context)));
-                                    }
-                                    Err(err) => {
-                                        log::error!(
-                                            "Failed to reconnect serial port {}: {}",
-                                            state.tty_path,
-                                            err
-                                        );
-                                        // Continue and don't leave/terminate the control loop!
-                                    }
-                                };
-                                Ok(state)
-                            });
-                            Either::B(reconnected_state)
-                        }
+                    Err((ctrl_loop, err)) => {
+                        log::info!("{:?}", ctrl_loop.state.measurements);
+                        Either::B(ctrl_loop.recover_after_error(&err))
                     }
                 })
         });
 
-    core.run(control_loop_task).unwrap();
+    core.run(ctrl_loop_task).unwrap();
 }
 
 #[cfg(not(feature = "modbus-rtu"))]
